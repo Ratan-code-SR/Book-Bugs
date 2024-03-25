fe7aa7bc4c0a58f118f9e53aab4de5291221e2f9
ratan <ratanchakma68@gmail.com>
2024-03-25 19:35:52 +0600

    project environment set up

---
+++ src/pages/Book_list.jsx
@@ -0,0 +1,11 @@
+
+
+const Book_list = () => {
+    return (
+        <div>
+            <h1>book list</h1>
+        </div>
+    );
+};
+
+export default Book_list;
---
+++ src/pages/Page_to_read.jsx
@@ -0,0 +1,11 @@
+
+
+const Page_to_read = () => {
+    return (
+        <div>
+            <h1>page to read</h1>
+        </div>
+    );
+};
+
+export default Page_to_read;
